c160c2f00b4b193c93bc74c93737477b72506d3d
David Ziegert <davidziegert@live.de>
2023-01-20 12:40:14 +0100

    refresh

--- php/languagechanger/de.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$lang = array(
-    "title" => "Deutsch.tmp",
-    "h1" => "Meine Webseite in Deutsch",
-    "text" => "Die ausgewählte Sprache ist"
-);
--- wordpress/wp_sfb1287_new/nav.php
+++
@@ -1,4 +0,0 @@
-<nav id="menu">
-    <!-- Custom-Navigation () from functions.php (WITH container_id OR container_class) -->
-    <?php wp_nav_menu(array('theme_location' => 'my-main-menu', 'container_class' => 'my-main-menu')); ?>
-</nav>
