56b4350a60a076af4d9795df270cacc65dddb790
David Ziegert <davidziegert@live.de>
2024-05-27 10:49:52 +0200

    delete

--- css/tags/header/2/js/desktop.js
+++
@@ -1,7 +0,0 @@
-window.onscroll = function (ev) {
-  if (window.scrollY > window.innerHeight - 200) {
-    document.getElementById("mountain-title").style.display = "none";
-  } else {
-    document.getElementById("mountain-title").style.display = "block";
-  }
-};
