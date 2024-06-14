95150575405f8a5164df1f1b94b33960927c3927
David Ziegert <davidziegert@live.de>
2024-06-14 10:32:28 +0200

    cleanup

--- php/languagechanger/de.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$lang = array(
-    "title" => "deutsch.tmp",
-    "h1" => "Meine Webseite in Deutsch",
-    "text" => "Die ausgewählte Sprache ist",
-    "icon" => "<span class='flag-icon flag-icon-de flag-icon-squared'></span>"
-);
--- php/languagechanger/fr.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$lang = array(
-    "title" => "français.tmp",
-    "h1" => "Mon site en français",
-    "text" => "La langue sélectionnée est",
-    "icon" => "<span class='flag-icon flag-icon-fr flag-icon-squared'></span>"
-);
