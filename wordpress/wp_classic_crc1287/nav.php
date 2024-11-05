c185cd0853abe98e3282c7a5cff556f362fcddbe
David Ziegert <davidziegert@live.de>
2024-11-05 13:42:47 +0100

    delete

--- wordpress/wp_classic_crc1287/nav.php
+++
@@ -1,7 +0,0 @@
-<nav id="nav">
-    <section>
-        <div class="row">
-            <?php wp_nav_menu(array('theme_location' => 'my-main-menu', 'container_class' => 'main-menu')); ?>
-        </div>
-    </section>
-</nav>
--- wordpress/wp_classic_crc1287/searchform.php
+++
@@ -1,5 +0,0 @@
-<!-- Search-Form-Template -->
-<form class="search-menu" method="get" action="<?php echo esc_url(home_url('/')); ?>">
-    <input type="text" placeholder="..." name="s" value="<?php echo get_search_query(); ?>">
-    <button type="submit"><i class="fa fa-search fa-lg"></i></button>
-</form>
