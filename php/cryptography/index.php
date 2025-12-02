a3641ec8443fd554ca4b1a983fd5258807746405
David Ziegert <davidziegert@live.de>
2025-12-02 10:53:16 +0100

    update

--- php/cryptography/index.php
+++ php/cryptography/index.php
@@ -52,10 +52,6 @@ function BCrypt_Verify_Password()
     }
 }
 
-?>
-
-<?php
-
 // for content only
 
 $plain_content = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.';
